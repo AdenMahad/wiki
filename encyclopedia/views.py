0966edebe3cc1c5e2a0e733c3e7c8be264a6d76b
aden <adenmahad@outlook.com>
2023-05-11 08:11:26 +0300

    adden url entry visits , generates a html page for each requested entry

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponseNotFound
 
 from . import util
 
@@ -7,4 +8,12 @@ def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
+
+def entry_page(request , name):
+    if util.get_entry(name) != None:
+        page = util.get_entry(name)
+        return render(request,"encyclopedia/entry.html",{"page":page,})
+    else:
+        return HttpResponseNotFound("<h1>Page not found</h1>")
+
 
